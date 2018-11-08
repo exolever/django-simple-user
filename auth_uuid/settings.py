d8de2f12a9456250b64b583fd99847e28378abfc
Unknown <tomasgarzonhervas@gmail.com>
2018-11-08 13:38:58 +0100

    Adding jwt build method

--- auth_uuid/settings.py
+++ auth_uuid/settings.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
 
 JWT_VERIFY_EXPIRATION = False
 JWT_VERIFY = True
@@ -12,6 +11,10 @@ JWT_AUTH = {
     'JWT_DECODE_HANDLER': 'auth_uuid.helper_jwt.jwt_decode_handler',
     'JWT_PAYLOAD_GET_USERNAME_HANDLER': 'auth_uuid.helper_jwt.jwt_get_username_from_payload_handler',
     'JWT_AUTH_HEADER_PREFIX': 'Bearer',
+    'JWT_VERIFY_EXPIRATION': False,
+    'JWT_RESPONSE_PAYLOAD_HANDLER':
+        'auth_uuid.jwt_response_payload_handler.jwt_response_payload_handler',
+    'JWT_PAYLOAD_HANDLER': 'auth_uuid.jwt_payload_handler.jwt_payload_handler',
 }
 
 JWT_SECRET_KEY = getattr(settings, 'JWT_SECRET_KEY', '')
@@ -22,4 +25,3 @@ URL_VALIDATE_USER_COOKIE = getattr(settings, 'URL_VALIDATE_USER_COOKIE', '')
 LOGGER_NAME = 'auth_uuid'
 
 settings.JWT_AUTH = JWT_AUTH
-
