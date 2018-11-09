ab3d82b70dcec794e96d4c7a88a511a0edc0026c
Unknown <tomasgarzonhervas@gmail.com>
2018-11-09 12:17:25 +0100

    Fix serialization

--- auth_uuid/jwt_payload_handler.py
+++ auth_uuid/jwt_payload_handler.py
@@ -5,7 +5,5 @@ def jwt_payload_handler(user):
     return {
         'user_id': user.id,
         'uuid': str(user.uuid),
-        'email': user.email,
-        'username': user.username,
         'exp': datetime(2100, 1, 1),
     }
