5bad67b77d94e75724277a77366bc9c7fb20f78f
Tomas <tomasgarzonhervas@gmail.com>
2019-04-02 10:24:20 +0200

    Bump version: 0.6.0 → 0.7.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='simple_user',
-    version='0.6.0',
+    version='0.7.0',
     description="""Simple reusable user storing only uuid and manage JWT Token auth_uuidentication in remote""",
     long_description=readme + '\n\n' + history,
     author='Tomas Garzon',
