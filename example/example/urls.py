85bdeb6c0b1d60003887c9121ce2b3c75327f6e7
Alvaro Molina Alvarez <alvaro@exolever.com>
2019-09-03 17:02:33 +0200

    fixing build

--- example/example/urls.py
+++ example/example/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('auth_uuid.urls', namespace='auth_uuid')),
+    url(r'', include('auth.urls', namespace='auth_uuid')),
 ]
